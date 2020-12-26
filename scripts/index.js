c6532154f568e780fb4f34f2f4556e80991b337f
ezinber <ezinber@yandex.ru>
2020-12-27 00:50:37 +0300

    fix: исправить замечания из ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,47 @@
+// ПЕРЕМЕННЫЕ
+
+// открытие/закрытие popup
 let openEdit = document.querySelector('.profile__edit-button');
 let closeEdit = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
+// изменение данных профиля через форму
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__text_name_name');
+let jobInput = formElement.querySelector('.form__text_name_job');
 
-function popupToggle() {
+// ФУНКЦИИ
+
+// открытие/закрытие popup
+function popupOpen() {
   if (event.target === event.currentTarget) {
-    popup.classList.toggle('popup_opened');
+    popup.classList.add('popup_opened');
+
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
   }
 }
 
-openEdit.addEventListener('click', popupToggle);
-closeEdit.addEventListener('click', popupToggle);
-
-let formElement = document.querySelector('.form');
-
+function popupClose() {
+  if (event.target === event.currentTarget) {
+    popup.classList.remove('popup_opened');
+  }
+}
+// изменение данных профиля через форму
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    let nameInput = formElement.querySelector('.form__text_name_name').value;
-    let jobInput = formElement.querySelector('.form__text_name_job').value;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
 
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-    profileTitle.textContent = nameInput;
-    profileSubtitle.textContent = jobInput;
-
-    popupToggle();
+    popupClose();
 }
 
+// СОБЫТИЯ
+
+// открытие/закрытие popup
+openEdit.addEventListener('click', popupOpen);
+closeEdit.addEventListener('click', popupClose);
+// изменение данных профиля через форму
 formElement.addEventListener('submit', handleFormSubmit);
